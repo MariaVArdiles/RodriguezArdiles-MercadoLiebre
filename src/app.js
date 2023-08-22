15ec986b7db793b1c77a719fc80879668f68ff32
Maria Rodriguez <mariavardiles@gmail.com>
2023-08-21 21:38:36 -0300

    agrego cambios en package.json y en src/app.js

--- src/app.js
+++ src/app.js
@@ -1,22 +1,22 @@
-const path = require('path')
+const path = require("path");
 
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, '../public')))
+app.use(express.static(path.join(__dirname, "../public")));
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`El servidor está escuchando en http://localhost:${port}`);
+  console.log(`El servidor está escuchando en http://localhost:${port}`);
 });
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, "views/home.html"));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "views/home.html"));
 });
-app.get('/register.html', (req, res) => {
-    res.sendFile(path.join(__dirname, "views/register.html"));
+app.get("/register.html", (req, res) => {
+  res.sendFile(path.join(__dirname, "views/register.html"));
 });
-app.get('/login.html', (req, res) => {
-    res.sendFile(path.join(__dirname, "views/login.html"));
+app.get("/login.html", (req, res) => {
+  res.sendFile(path.join(__dirname, "views/login.html"));
 });
